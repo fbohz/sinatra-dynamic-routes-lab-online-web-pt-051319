c2c6655be8dbf413ea1dffad237cf57d3a93812f
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-08-05 19:33:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     (num.to_i**2).to_s #run operation first then turn it back to string to pass test. 
   end
   
-   get '/say/:number/:phrase' do
+   get '/say/:number/:phrase' do #trickiest method
     string = ""
     (params[:number].to_i).times do
       string += "#{params[:phrase]}\n"
@@ -19,7 +19,9 @@ class App < Sinatra::Base
     string
   end
   
-  
+    get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
 
 
 end
