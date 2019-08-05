5e4af9e2305c6c7b0ac5d98d973865fc2abf789a
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-08-05 19:29:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   
    get '/square/:number' do
     num = params[:number]
-    (num.to_i**2).to_s
+    (num.to_i**2).to_s #run operation first then turn it back to string. 
   end
 
 end
