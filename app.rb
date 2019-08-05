dc09b02c3d0b3c7bb8f666bd0cbb02772b991a87
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-08-05 19:29:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   
    get '/square/:number' do
     num = params[:number]
-    (num.to_i**2).to_s #run operation first then turn it back to string. 
+    (num.to_i**2).to_s #run operation first then turn it back to string to pass test. 
   end
 
 end
